077964cde9007f65d43ee953f5955e462f3e6fb9
Elif Balci <mrselifbalci@gmail.com>
2022-04-28 22:57:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+import './App.css';
+
+function App() {
+  const [weather,setWeather]=useState()
+  let apiKey=process.env.REACT_APP_API_KEY
+  let search = 'london';
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`)
+  .then(response=> response.json())
+  .then(data=>{
+     setWeather(data.weather[0].main)
+ });
+  return (
+    <div className="App">
+      <h1>{weather}</h1> 
+    </div>
+  );
+}
+
+export default App;
